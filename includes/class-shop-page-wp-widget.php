44d62ae9f86aa2ac3b581aec204b94d47cfd2653
Leon Magee <leonmagee@hotmail.com>
2017-03-20 12:25:14 -0700

    update

--- includes/class-shop-page-wp-widget.php
+++ includes/class-shop-page-wp-widget.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * Adds Foo_Widget widget.
+ * Class Shop_Page_WP_Widget
  */
 class Shop_Page_WP_Widget extends WP_Widget {
 
 	/**
-	 * Register widget with WordPress.
+	 * Register widget
 	 */
 	function __construct() {
 		parent::__construct(
 			'shop_page_wp',
 			esc_html__( 'Shop Page WP', 'shop-page-wp' ),
-			array( 'description' => esc_html__( 'Output Shop Page WP responsive product grid.', 'shop-page-wp' ), ) // Args
+			array( 'description' => esc_html__( 'Output Shop Page WP responsive product grid.', 'shop-page-wp' ), )
 		);
 	}
 
@@ -33,12 +33,7 @@ class Shop_Page_WP_Widget extends WP_Widget {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
 
-		$attributes = array(
-			'category' => '',
-			'grid'     => 1
-		);
-
-		$grid_content = Shop_Page_WP_Grid::return_grid( $attributes );
+		$grid_content = Shop_Page_WP_Grid::return_grid( $instance );
 
 		echo $grid_content;
 
@@ -53,14 +48,46 @@ class Shop_Page_WP_Widget extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( '', 'shop-page-wp' );
+
+		$columns = ! empty( $instance['grid'] ) ? $instance['grid'] : 1;
+
+		$categories = ! empty( $instance['category'] ) ? $instance['category'] : esc_html__( '', 'shop-page-wp' );
 		?>
 		<p>
 			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label>
+				for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'shop-page-wp' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text"
 			       value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<p>
+			<label
+				for="<?php echo esc_attr( $this->get_field_id( 'grid' ) ); ?>"><?php esc_attr_e( 'Columns:', 'shop-page-wp' ); ?></label>
+			<select class="widefat"
+			        id="<?php echo esc_attr( $this->get_field_id( 'grid' ) ); ?>"
+			        name="<?php echo esc_attr( $this->get_field_name( 'grid' ) ); ?>">
+				<option
+					value="1" <?php selected( $columns, '1' ); ?>><?php _e( '1 Column', 'shop-page-wp' ); ?>
+				</option>
+				<option
+					value="2" <?php selected( $columns, '2' ); ?>><?php _e( '2 Column', 'shop-page-wp' ); ?>
+				</option>
+				<option
+					value="3" <?php selected( $columns, '3' ); ?>><?php _e( '3 Column', 'shop-page-wp' ); ?>
+				</option>
+				<option
+					value="4" <?php selected( $columns, '4' ); ?>><?php _e( '4 Column', 'shop-page-wp' ); ?>
+				</option>
+			</select>
+		</p>
+		<p>
+			<label
+				for="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"><?php esc_attr_e( 'Categories (Separate multiple with pipe | symbol)', 'shop-page-wp' ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"
+			       name="<?php echo esc_attr( $this->get_field_name( 'category' ) ); ?>" type="text"
+			       value="<?php echo esc_attr( $categories ); ?>">
 		</p>
 		<?php
 	}
@@ -76,10 +103,11 @@ class Shop_Page_WP_Widget extends WP_Widget {
 	 * @return array Updated safe values to be saved.
 	 */
 	public function update( $new_instance, $old_instance ) {
-		$instance          = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance             = array();
+		$instance['title']    = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['grid']     = ( ! empty( $new_instance['grid'] ) ) ? strip_tags( $new_instance['grid'] ) : 1;
+		$instance['category'] = ( ! empty( $new_instance['category'] ) ) ? strip_tags( $new_instance['category'] ) : '';
 
 		return $instance;
 	}
-
-} // class Foo_Widget
+}
